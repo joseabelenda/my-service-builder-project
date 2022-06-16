ac17efb3b1b9115d9a239e7f6b5ce96031dfdef0
Jose Abelenda <jose.abelenda@liferay.com>
2022-06-16 19:37:59 -0300

    add getter methods that invoke the generated finder methods on H7G5EntryLocalServiceImpl

--- h7g5-service/src/main/java/com/liferay/h7g5/service/impl/H7G5EntryLocalServiceImpl.java
+++ h7g5-service/src/main/java/com/liferay/h7g5/service/impl/H7G5EntryLocalServiceImpl.java
@@ -14,6 +14,8 @@
 
 package com.liferay.h7g5.service.impl;
 
+import com.liferay.h7g5.exception.NoSuchH7G5EntryException;
+import com.liferay.h7g5.model.H7G5Entry;
 import com.liferay.h7g5.service.base.H7G5EntryLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 
@@ -27,4 +29,22 @@ import org.osgi.service.component.annotations.Component;
 	service = AopService.class
 )
 public class H7G5EntryLocalServiceImpl extends H7G5EntryLocalServiceBaseImpl {
+
+	public java.util.List<H7G5Entry> findByH7G5FolderId(long h7g5FolderId) {
+		return h7g5EntryPersistence.findByH7G5FolderId(h7g5FolderId);
+	}
+
+	public java.util.List<H7G5Entry> findByName(String name) {
+		return h7g5EntryPersistence.findByName(name);
+	}
+
+	public H7G5Entry findByKey(String key) throws NoSuchH7G5EntryException {
+		return h7g5EntryPersistence.findByKey(key);
+	}
+
+	public H7G5Entry findByH_D_N(
+			long h7g5FolderId, String description, String name)
+		throws NoSuchH7G5EntryException {
+		return h7g5EntryPersistence.findByH_D_N(h7g5FolderId, description, name);
+	}
 }
